16c7b4c9d0cff53c7b35e4ad2523cf832b6eb2b8
Hendrik Wolff <hendrik.wolff@agdsn.me>
2021-08-06 19:07:25 +0200

    Add advanced serial connection settings
    - data bits
    - parity
    - stop bits
    - flow control
    
    Extend the help messages

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use log::trace;
+use serialport::SerialPortBuilder;
+use serialport::{DataBits, FlowControl, Parity, StopBits};
 use std::io::{stdin, stdout, Read, Write};
 use std::sync::{Arc, Mutex};
 use std::thread;
@@ -9,15 +11,74 @@ use termion::raw::IntoRawMode;
 use termion::screen::*;
 
 #[derive(Debug, StructOpt)]
-#[structopt(author)]
+#[structopt(author, global_setting(structopt::clap::AppSettings::ColoredHelp))]
 struct SC {
-    /// Baud rate to connect at
-    #[structopt(name = "baud rate", short, long = "baudrate", default_value = "9600")]
-    baud_rate: u32,
-
-    /// Device path to a serial port
+    /// Sets the device path to a serial port
     #[structopt(parse(from_str))]
     device: String,
+
+    /// Sets the baud rate to connect at
+    #[structopt(
+        name = "baud rate",
+        default_value = "9600",
+        long_help = r"Sets the baud rate to connect at
+
+Common values: 300, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200, 230400, 460800, 500000, 576000, 921600, 1000000, 1152000, 1500000, 2000000, 2500000, 3000000, 3500000, 4000000
+"
+    )]
+    baud_rate: u32,
+
+    /// Sets the number of bits used per character
+    #[structopt(
+        name = "data bits",
+        default_value = "8",
+        long_help = r"Sets the number of bits per character
+
+Possible values:
+    - 5
+    - 6
+    - 7
+    - 8
+"
+    )]
+    data_bits: u8,
+    /// Sets the parity checking mode
+    #[structopt(
+        default_value = "None",
+        long_help = r"Sets the parity checking mode
+
+Possible values:
+    - None, N, n => None
+    - Odd,  O, o => Odd
+    - Even, E, e => Even
+"
+    )]
+    parity: String,
+    /// Sets the number of stop bits transmitted after every character
+    #[structopt(
+        name = "stop bits",
+        default_value = "1",
+        long_help = r"Sets the number of stop bits transmitted after every character
+
+Possible values:
+    - 1
+    - 2
+"
+    )]
+    stop_bits: u8,
+    /// Sets the flow control mode
+    #[structopt(
+        name = "flow control",
+        default_value = "None",
+        long_help = r"Sets the flow control mode
+
+Possible values:
+    - None, N, n => None
+    - Hardware, Hard, H, h => Hardware    # uses XON/XOFF bytes
+    - Software, Soft, S, s => Software    # uses RTS/CTS signals
+"
+    )]
+    flow_control: String,
 }
 
 fn write_start_screen_msg<W: Write>(screen: &mut Arc<Mutex<W>>) {
@@ -35,43 +96,28 @@ fn write_start_screen_msg<W: Write>(screen: &mut Arc<Mutex<W>>) {
 }
 
 fn main() {
-    let sc_opt: SC = SC::from_args();
+    let sc_args: SC = SC::from_args();
+
+    let port: SerialPortBuilder = parse_arguments_into_serialport(&sc_args);
+    let port = match port.open() {
+        Ok(sp) => sp,
+        Err(err) if err.kind() == serialport::ErrorKind::Io(std::io::ErrorKind::NotFound) => {
+            eprint!("Device not found: {}\n\r", sc_args.device);
+            std::process::exit(1);
+        }
+        Err(err) => {
+            eprint!("Error opening port, please report this: {:?}\n\r", err);
+            std::process::exit(1);
+        }
+    };
+    let mut serial_port_in = port.try_clone().unwrap();
+    let mut serial_port_out = port.try_clone().unwrap();
 
     let mut stdin = stdin();
     let screen = AlternateScreen::from(stdout().into_raw_mode().unwrap());
     let screen = Arc::new(Mutex::new(screen));
 
     write_start_screen_msg(&mut screen.clone());
-
-    // let ports = serialport::available_ports().expect("No ports found!");
-    // for p in ports {
-    //     write!(screen.lock().unwrap(), "{}\n\r", p.port_name).unwrap();
-    // }
-    // screen.lock().unwrap().flush().unwrap();
-
-    let port = serialport::new(sc_opt.device.clone(), sc_opt.baud_rate)
-        .timeout(Duration::from_millis(1))
-        .open();
-
-    let port = match port {
-        Ok(sp) => sp,
-        Err(err) if err.kind() == serialport::ErrorKind::Io(std::io::ErrorKind::NotFound) => {
-            eprint!("{}Port not Found: {}\n\r", ToMainScreen, sc_opt.device);
-            screen.lock().unwrap().suspend_raw_mode().unwrap();
-            std::process::exit(1);
-        }
-        Err(err) => {
-            eprint!(
-                "{}Error opening port, please report this: {:?}\n\r",
-                ToMainScreen, err
-            );
-            screen.lock().unwrap().suspend_raw_mode().unwrap();
-            std::process::exit(1);
-        }
-    };
-
-    let mut serial_port_in = port.try_clone().unwrap();
-    let mut serial_port_out = port.try_clone().unwrap();
 
     let screen_from = screen.clone();
     let _from_serial_handle = thread::spawn(move || loop {
@@ -88,6 +134,7 @@ fn main() {
                 }
             }
             Err(ref err) if err.kind() == std::io::ErrorKind::TimedOut => {}
+            // todo: catch broken pipe error
             Err(err) => {
                 eprint!("{}{}\n\r", ToMainScreen, err);
                 break;
@@ -114,4 +161,41 @@ fn main() {
     _from_serial_handle.join().unwrap();
     // _to_serial_handle.join();
     screen.lock().unwrap().flush().unwrap();
+}
+
+fn parse_arguments_into_serialport(sc_args: &SC) -> SerialPortBuilder {
+    let path: String = sc_args.device.clone();
+    let baud_rate: u32 = sc_args.baud_rate;
+    let data_bits: DataBits = match sc_args.data_bits {
+        8 => DataBits::Eight,
+        7 => DataBits::Seven,
+        6 => DataBits::Six,
+        5 => DataBits::Five,
+        _ => DataBits::Eight,
+    };
+    let parity: Parity = match sc_args.parity.as_str() {
+        "None" | "N" | "n" => Parity::None,
+        "Odd" | "O" | "o" => Parity::Odd,
+        "Even" | "E" | "e" => Parity::None,
+        _ => Parity::None,
+    };
+    let stop_bits: StopBits = match sc_args.stop_bits {
+        1 => StopBits::One,
+        2 => StopBits::Two,
+        _ => StopBits::One,
+    };
+    let flow_control: FlowControl = match sc_args.flow_control.as_str() {
+        "None" | "N" | "n" => FlowControl::None,
+        "Hardware" | "Hard" | "H" | "h" => FlowControl::Hardware,
+        "Software" | "Soft" | "S" | "s" => FlowControl::Software,
+        _ => FlowControl::None,
+    };
+    let timeout: Duration = Duration::from_millis(10);
+
+    serialport::new(path, baud_rate)
+        .data_bits(data_bits)
+        .parity(parity)
+        .stop_bits(stop_bits)
+        .flow_control(flow_control)
+        .timeout(timeout)
 }
